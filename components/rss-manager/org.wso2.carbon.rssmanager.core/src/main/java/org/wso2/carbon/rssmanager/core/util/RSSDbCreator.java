ca8b4b4f39db4ef800cc806bcb86f9113fb340af
Poshitha Dabare <prd4008@yahoo.com>
2014-08-10 14:33:11 +0530

    Update RSSDbCreator.java
    added licence comment

--- components/rss-manager/org.wso2.carbon.rssmanager.core/src/main/java/org/wso2/carbon/rssmanager/core/util/RSSDbCreator.java
+++ components/rss-manager/org.wso2.carbon.rssmanager.core/src/main/java/org/wso2/carbon/rssmanager/core/util/RSSDbCreator.java
@@ -1,3 +1,22 @@
+/*
+* Copyright (c) 2013, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*
+*/
+
 package org.wso2.carbon.rssmanager.core.util;
 
 import javax.sql.DataSource;
@@ -9,12 +28,12 @@ import org.wso2.carbon.utils.dbcreator.DatabaseCreator;
 public class RSSDbCreator extends DatabaseCreator {
 
     private static final Log log = LogFactory.getLog(RSSDbCreator.class);
-    public String dbDir = "";
-	public RSSDbCreator(DataSource dataSource) {
+    public String dbDir = "";  // stores the the location of the database script that is run according to the databse type
+    
+    public RSSDbCreator(DataSource dataSource) {
 		super(dataSource);
 		
-		// TODO Auto-generated constructor stub
-	}
+    }
 	
     protected String getDbScriptLocation(String databaseType) {
 	        String scriptName = "wso2_rss_" + databaseType + ".sql";
@@ -23,6 +42,7 @@ public class RSSDbCreator extends DatabaseCreator {
 	        }
 	        String carbonHome = System.getProperty("carbon.home");
 	        return dbDir.replaceFirst("DBTYPE", databaseType)  + scriptName;
-	    }
+    	
+    }
 
 }
